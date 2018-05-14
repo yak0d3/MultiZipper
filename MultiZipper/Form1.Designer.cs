48f07be7802d9b68214dbaa7ad55b8b32872f913
yaKode <32985181+yaKode@users.noreply.github.com>
2018-05-14 05:19:42 +0000

    Add files via upload

---
+++ MultiZipper/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MultiZipper
+{
+    partial class multizipper_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statLabel = new System.Windows.Forms.Label();
+            this.browseBtn1 = new System.Windows.Forms.Button();
+            this.browseBtn2 = new System.Windows.Forms.Button();
+            this.maindirLabel = new System.Windows.Forms.Label();
+            this.outputsLabel = new System.Windows.Forms.Label();
+            this.extensionLabel = new System.Windows.Forms.Label();
+            this.extBox = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // statLabel
+            // 
+            this.statLabel.AutoSize = true;
+            this.statLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.statLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.statLabel.Location = new System.Drawing.Point(12, 244);
+            this.statLabel.Name = "statLabel";
+            this.statLabel.Size = new System.Drawing.Size(35, 20);
+            this.statLabel.TabIndex = 0;
+            this.statLabel.Text = "Idle";
+            // 
+            // browseBtn1
+            // 
+            this.browseBtn1.Location = new System.Drawing.Point(11, 41);
+            this.browseBtn1.Name = "browseBtn1";
+            this.browseBtn1.Size = new System.Drawing.Size(146, 23);
+            this.browseBtn1.TabIndex = 1;
+            this.browseBtn1.Text = "BROWSE";
+            this.browseBtn1.UseVisualStyleBackColor = true;
+            this.browseBtn1.Click += new System.EventHandler(this.browseBtn1_Click);
+            // 
+            // browseBtn2
+            // 
+            this.browseBtn2.Location = new System.Drawing.Point(10, 93);
+            this.browseBtn2.Name = "browseBtn2";
+            this.browseBtn2.Size = new System.Drawing.Size(146, 23);
+            this.browseBtn2.TabIndex = 2;
+            this.browseBtn2.Text = "BROWSE";
+            this.browseBtn2.UseVisualStyleBackColor = true;
+            this.browseBtn2.Click += new System.EventHandler(this.browseBtn2_Click);
+            // 
+            // maindirLabel
+            // 
+            this.maindirLabel.AutoSize = true;
+            this.maindirLabel.Location = new System.Drawing.Point(12, 20);
+            this.maindirLabel.Name = "maindirLabel";
+            this.maindirLabel.Size = new System.Drawing.Size(78, 13);
+            this.maindirLabel.TabIndex = 3;
+            this.maindirLabel.Text = "Main Directory:";
+            // 
+            // outputsLabel
+            // 
+            this.outputsLabel.AutoSize = true;
+            this.outputsLabel.Location = new System.Drawing.Point(11, 72);
+            this.outputsLabel.Name = "outputsLabel";
+            this.outputsLabel.Size = new System.Drawing.Size(92, 13);
+            this.outputsLabel.TabIndex = 4;
+            this.outputsLabel.Text = "Outputs Directory:";
+            // 
+            // extensionLabel
+            // 
+            this.extensionLabel.AutoSize = true;
+            this.extensionLabel.Location = new System.Drawing.Point(11, 124);
+            this.extensionLabel.Name = "extensionLabel";
+            this.extensionLabel.Size = new System.Drawing.Size(105, 13);
+            this.extensionLabel.TabIndex = 5;
+            this.extensionLabel.Text = "Extension (optional): ";
+            // 
+            // extBox
+            // 
+            this.extBox.Location = new System.Drawing.Point(14, 145);
+            this.extBox.Name = "extBox";
+            this.extBox.Size = new System.Drawing.Size(143, 20);
+            this.extBox.TabIndex = 6;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(13, 176);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(144, 23);
+            this.startBtn.TabIndex = 7;
+            this.startBtn.Text = "START";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Enabled = false;
+            this.stopBtn.Location = new System.Drawing.Point(13, 205);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(144, 23);
+            this.stopBtn.TabIndex = 8;
+            this.stopBtn.Text = "STOP";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // multizipper_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(173, 311);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.extBox);
+            this.Controls.Add(this.extensionLabel);
+            this.Controls.Add(this.outputsLabel);
+            this.Controls.Add(this.maindirLabel);
+            this.Controls.Add(this.browseBtn2);
+            this.Controls.Add(this.browseBtn1);
+            this.Controls.Add(this.statLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "multizipper_form";
+            this.Text = "MultiZipper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label statLabel;
+        private System.Windows.Forms.Button browseBtn1;
+        private System.Windows.Forms.Button browseBtn2;
+        private System.Windows.Forms.Label maindirLabel;
+        private System.Windows.Forms.Label outputsLabel;
+        private System.Windows.Forms.Label extensionLabel;
+        private System.Windows.Forms.TextBox extBox;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+    }
+}
+
---
+++ MultiZipper/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.IO;
+using System.Threading;
+
+namespace MultiZipper
+{
+    public partial class multizipper_form : Form
+    {
+        public multizipper_form()
+        {
+            InitializeComponent();
+
+            bgw.WorkerReportsProgress = true;
+            bgw.WorkerSupportsCancellation = true;
+
+            bgw.DoWork += delegate
+            {
+                try
+                {
+                    ThreadPool.SetMinThreads(400,400); // You can make edits for the number of concurrent threads to be used. (Updates will be added in the next versions.
+                    Parallel.ForEach(Directory.GetFiles(mainDir,extension).Select(Path.GetFileName), file =>
+                    {
+
+                            createZip(mainDir, oDir, file);
+                            d++;
+                            p++;
+                            bgw.ReportProgress(p);
+                    });
+                }
+                catch (Exception ex)
+                {
+                    f++;
+                    p++;
+                    bgw.ReportProgress(p);
+                   // MessageBox.Show(ex.ToString()); // Remove '//' to activate the erros display.
+                }
+            };
+            bgw.ProgressChanged += delegate
+            {
+                statLabel.Text = "Succeeded: "+d+"\nFailed: "+f+"\nProcessed: "+p+"/"+t; //updating the status label
+            };
+
+            bgw.RunWorkerCompleted += delegate
+            {
+                startBtn.Enabled = true;
+                stopBtn.Enabled = false;
+                browseBtn1.Enabled = true;
+                browseBtn2.Enabled = true;
+            };
+        }
+
+
+        private BackgroundWorker bgw = new BackgroundWorker();
+
+        #region counters
+        /* d for Succeeded, p for Processed, f for Failed and t for Total */
+        private int d,p,f,t = 0;
+        #endregion
+
+        #region paths_variables
+        private string mainDir = String.Empty; //main directory
+        private string oDir = String.Empty; //outputs directory
+        #endregion
+
+        #region folderBrowsingDialogs
+        private FolderBrowserDialog fbd1 = new FolderBrowserDialog();
+        private FolderBrowserDialog fbd2 = new FolderBrowserDialog();
+        #endregion
+
+        private string extension = String.Empty;
+
+        private void browseBtn1_Click(object sender, EventArgs e)
+        {
+            if (fbd1.ShowDialog() == DialogResult.OK)
+            {
+                t = Directory.GetFiles(fbd1.SelectedPath).Count();
+                mainDir = fbd1.SelectedPath;
+            }
+        }
+
+        private void browseBtn2_Click(object sender, EventArgs e)
+        {
+            
+            if (fbd2.ShowDialog() == DialogResult.OK)
+            {
+                t = Directory.GetFiles(fbd2.SelectedPath).Count();
+                oDir = fbd2.SelectedPath;
+            }
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            bgw.Dispose();
+            bgw.CancelAsync();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(fbd1.SelectedPath.Trim() != "")
+            {
+                if(fbd2.SelectedPath.Trim() != "")
+                {
+                    startBtn.Enabled = false;
+                    stopBtn.Enabled = true;
+                    browseBtn1.Enabled = false;
+                    browseBtn2.Enabled = false;
+
+                    if(extBox.Text.Trim() != "")
+                    {
+                        extension = "*." + extBox.Text.Replace(".","");
+                    }
+                    else
+                    {
+                        extension = "*";
+                    }
+                    d = 0;
+                    p = 0;
+                    f = 0;
+                    t = Directory.GetFiles(mainDir,extension).Select(Path.GetFileName).Count();
+
+                    bgw.RunWorkerAsync();
+                }
+                else
+                {
+                    MessageBox.Show("Please select the outputs directory.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select the directory containing your files.");
+            }
+        }
+
+        
+        private void createZip(string main_path,string opath,string filename)
+        {
+                Directory.CreateDirectory(opath + @"\~" + filename);
+
+                File.Copy(main_path + @"\" + filename, opath + @"\~" + filename + @"\" + filename);
+
+                ZipFile.CreateFromDirectory(opath + @"\~" + filename, opath + @"\" + Path.GetFileNameWithoutExtension(filename) + ".zip");
+
+                Directory.Delete(opath + @"\~" + filename, true);
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
